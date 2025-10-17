071dded482f99db40d342685174a2987a9ff174e
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2025-10-17 23:06:54 +0300

    changes

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,15 @@ import path from 'path';
 
 export default defineConfig({
   server: {
-    port: 5173,
+    host: '0.0.0.0',
+    port: 5000,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
   },
   build: {
     outDir: 'dist',
