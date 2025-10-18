7c165f5d56d1885b5cadd472f3dad6f39bd4d678
MarieMiatova <146247021+MarieMiatova@users.noreply.github.com>
2025-10-18 18:52:40 +0300

    Update database.py

--- backend/app/database.py
+++ backend/app/database.py
@@ -2,10 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 import os
 
-# Ожидается что DATABASE_URL будет вида:
-# postgresql://user:pass@host:5432/dbname
+
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:pass@localhost:5432/calc")
 
-# create_engine без future чтобы работать со старой накаткой sqlalchemy
+
 engine = create_engine(DATABASE_URL, future=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
